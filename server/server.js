9fadca156fd4a0f08942b23e683c2a6a8257fcbb
S M SUTHARSAN RAJ <77114267+smsraj2001@users.noreply.github.com>
2024-01-19 18:44:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,5 @@
-// // server/server.js
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const bookRoutes = require('./routes/bookRoutes');
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json()); // Use express.json() to handle JSON payloads
-
-// // Connect to MongoDB with updated options
-// mongoose.connect('mongodb://localhost:27017/book', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   // Remove the useCreateIndex option
-// });
-
-// // Use routes
-// app.use('/api/books', bookRoutes);
-
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
 // server/server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
